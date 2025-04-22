a7a704cdb5c6462cc04a8ceacfdc8ac637982556
seodaeryong <wotnek2@naver.com>
2025-04-22 21:44:49 +0900

    [add] change close data

--- server.py
+++ server.py
@@ -9,9 +9,21 @@ def double(ticker: str = Query(..., description="문자열을 입력하세요"))
     total_return, trades, signal, df = backtest_with_stop_loss(ticker, start="2025-01-01")
     b_data = []
     s_data = []
-    for b, s in zip(df.to_dict()['buy_signal_rate'].values(), df.to_dict()['sell_signal_rate'].values()):
+    c_data = []
+    
+    for b, s, c in zip(df.to_dict()['buy_signal_rate'].values(), df.to_dict()['sell_signal_rate'].values(), df.to_dict()['Close'].values()):
         b_data.append(b)
         s_data.append(s)
-    b_data.reverse()
-    s_data.reverse()
-    return {"ticker": ticker, "total_return": total_return, "signal": signal.to_dict(), 'b_data': b_data[:20], 's_data': s_data[:20]}
+        c_data.append(c)
+
+    tmp = []
+    for index in range(0, len(c_data) - 1):
+        tmp.append(c_data[index+1] - c_data[index])
+    c_data = tmp
+
+    #print(df.to_dict()['Close'])
+    #print(df.to_dict()['buy_signal_rate'])
+    #print(df.to_dict()['sell_signal_rate'])
+
+    chart = {"b_data": b_data[-20:], "s_data": s_data[-20:], "c_data": c_data[-20:]}
+    return {"ticker": ticker, "total_return": total_return, "signal": signal.to_dict(), "chart": chart}
